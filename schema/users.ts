e7183cfc1c6643543e79213d645e591782bc8c1d
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-29 11:18:27 +0800

    added JSON Schema for POST users

---
+++ schema/users.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi'
+
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/
+
+export const userSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(15).required(),
+    password: Joi.string().pattern(passwordRegex).required(),
+    email: Joi.string().pattern(emailRegex).required(),
+    first_name: Joi.string().max(50).required(),
+    last_name: Joi.string().max(50).required()
+})
