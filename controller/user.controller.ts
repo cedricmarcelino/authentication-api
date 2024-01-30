9a5f2f3d9d082cc56ac5a977e9d94fbe5e9a9761
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-30 10:19:39 +0800

    added creation of token upon succesful sign up and adjusted method names for users.

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -1,18 +1,21 @@
 import { db } from '../db'
 import { Request, Response } from 'express'
-import { ValidationError } from 'types-joi' 
 import { userSchema } from '../schema/users'
 import { IUser } from '../db/models'
-const bcrypt = require('bcrypt')
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-const hashPassword = async (password: string) => {
-    try {
-      const salt = await bcrypt.genSalt(10)
-      const hashedPassword = await bcrypt.hash(password,salt)
-      return hashedPassword;
-    } catch(error) {
-        console.log(error)
-    }
+const maxAge = 24 * 60 * 60
+const createToken = (username: string) => {
+    return jwt.sign({ username }, 'this is a sample secret', {
+        expiresIn: maxAge
+    })
+}
+
+const hashPassword = (password: string) => {
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password,salt)
+    return hashedPassword;
   }
 
 const users_index = (_req: Request, res: Response) => {
@@ -36,23 +39,25 @@ const users_add = async (req: Request, res: Response) => {
         console.log('Request body valid.  \n', body)
         const { username, password, email, first_name, last_name } = data
         console.log('Checking if username exists.')
-        const isUsernameTaken = await db.users.isUsernameTaken(username)
+        const isUsernameTaken = await db.users.getUserByUsername(username)
         if (isUsernameTaken.length > 0) {
             console.log('Username is already taken.')
             return res.status(409).send('Username is already taken.')
         }
         console.log('Username is not taken.')
         console.log('Checking if email is already in use.')
-        const isEmailTaken = await db.users.isEmailTaken(email)
+        const isEmailTaken = await db.users.getUserByEmail(email)
         if (isEmailTaken.length > 0) {
             console.log('Email is already linked to an account.')
             return res.status(409).send('Email is already linked to an account.')
         }
         console.log('Email is not taken.')
         console.log('Encrypting password')
-        const hashedPassword = await hashPassword(password)
+        const hashedPassword = hashPassword(password)
         console.log('Password encrypted.')
         db.users.add(username, hashedPassword, email, first_name, last_name).then((data) => {
+            const token = createToken(data.username)
+            res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
             res.send(data)
         })
     } catch (error: any) {
@@ -68,4 +73,6 @@ const users_add = async (req: Request, res: Response) => {
 module.exports = {
   users_index,
   users_add,
+  maxAge,
+  createToken,
 }
