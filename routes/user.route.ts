ebc3579a96eb366d0e7e2ab374ba057048b6fda9
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-30 10:18:47 +0800

    renamed route for adding users

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -4,6 +4,6 @@ const userController = require('../controller/user.controller');
 const router = express.Router();
 
 router.get('/', userController.users_index);
-router.post('/', userController.users_add)
+router.post('/register', userController.users_add)
 
 module.exports = router;
