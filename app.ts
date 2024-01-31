b5daeb3e66fc60a1347739b9fe79659ee5f3f095
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-02-01 07:43:17 +0800

    added initial swagger docs

--- app.ts
+++ app.ts
@@ -1,26 +1,47 @@
 import express, { Express, Request, Response } from 'express';
 import { requestLogger } from './utils/loggerFactory'
+import { setup, SwaggerOptions, serve } from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc';
 const userRoutes = require('./routes/user.route');
 const authRoutes = require('./routes/auth.route');
 const cookieParser = require('cookie-parser');
 const logger = require('pino')();
-const app: Express = express()
 
 // TO DO:
-// Fix error handling for bad json format
-// Add pagination for all users
-// Try to catch error of JWT verify
 // Add json format for responses, on error and on success
 // Double check responses, make sure to log it.
 // Configure Swagger UI
+// Add pagination for all users
+// Try to catch error of JWT verify
+// Fix error handling for bad json format
+// Fix hard coded port and hosts
+
+const options: SwaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Authentication API',
+      version: '1.0.0',
+      description: `This is an authentication REST API and this is the first REST API I've made with NodeJS/Express.`
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ],
+  },
+  apis: ["./routes/*.ts"] // change to .js when going to deploy
+}
+
+const specs = swaggerJSDoc(options)
+
+const app: Express = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(requestLogger);
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Route for Swagger UI');
-});
+app.use('/explorer', serve, setup(specs));
 
 app.use('/users', userRoutes);
 app.use(authRoutes)
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -3,8 +3,55 @@ const userController = require('../controller/user.controller');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *  tags:
+ *   name: Users
+ *   description: Endpoints related to Users.
+ */
+
+/**
+ * @swagger
+ *  /users:
+ *   get:
+ *    summary: Returns all of the users.
+ *    tags: [Users]
+ *   post:
+ *    summary: Register a new user.
+ *    tags: [Users]
+ *    requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: object
+ *        properties: 
+ *         username:
+ *          type: string
+ *         password:
+ *          type: string
+ *         email:
+ *          type: string
+ *         first_name:
+ *          type: string
+ *         last_name:
+ *          type: string
+ *        required: 
+ *         username
+ *         password
+ *         email
+ *         first_name
+ *         last_name
+ *        example:
+ *         username: cedricmarcelino
+ *         password: SecuredPassword0711!
+ *         email: cedric@email.com
+ *         first_name: Cedric
+ *         last_name: Marcelino
+ * 
+ */
 router.get('/', userController.users_index);
-router.post('/add', userController.users_add)
+router.post('/', userController.users_add)
 router.get('/:username', userController.users_getByUsername)
 
 module.exports = router;
